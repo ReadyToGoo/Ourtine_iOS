8c4a760ed9cd18102cf01dba6bcfa8aa016c766e
PARKMINSEON <137928680+thisisminseon@users.noreply.github.com>
2023-07-28 14:59:43 +0900

    profileViewController

---
+++ Ourtine/Ourtine/ViewController/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Ourtine
+//
+//  Created by 박민선 on 2023/07/27.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
