8daf610338391568e7706e3e303301a1ea019f21
EunJI <83203198+ejalice@users.noreply.github.com>
2023-08-03 12:53:13 +0900

    [Feat] #22 FigmaShadow 적용 Extension 추가

---
+++ Ourtine/Ourtine/Extension/FigmaShadow.swift
@@ -0,0 +1,31 @@
+//
+//  FigmaShadow.swift
+//  Ourtine
+//
+//  Created by eunji on 2023/08/03.
+//
+
+import UIKit
+
+extension CALayer {
+    func applyFigmaShadow(
+        color: UIColor,
+        alpha: Float,
+        x: CGFloat,
+        y: CGFloat,
+        blur: CGFloat,
+        spread: CGFloat
+    ) {
+        masksToBounds = false
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / UIScreen.main.scale
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let rect = bounds.insetBy(dx: -spread, dy: -spread)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
