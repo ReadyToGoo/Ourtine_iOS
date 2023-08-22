a584412cc7af579a8fb4ee157cbd83d8af1e3fca
EunJI <83203198+ejalice@users.noreply.github.com>
2023-08-23 00:56:57 +0900

    [Feat] #66 영상 재생 및 타임라인 생성

--- Ourtine/Ourtine/ViewController/OthersViewController.swift
+++ Ourtine/Ourtine/ViewController/OthersViewController.swift
@@ -6,26 +6,95 @@
 //
 
 import UIKit
+import SnapKit
+import AVKit
 
-// 타인의 영상 확인
 class OthersViewController: UIViewController {
-
+    var selectedMember: MemberModel?
+    var player: AVPlayer?
+    var timeObserverToken: Any?
+    
+    var videoURL = URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")
+//    var videoURL = URL(string: "https://techslides.com/demos/sample-videos/small.mp4")
+    
+    let timelineProgressBar: UIProgressView = {
+        let progressBar = UIProgressView()
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        progressBar.progressTintColor = .white
+        progressBar.trackTintColor = .white.withAlphaComponent(0.4)
+    
+        return progressBar
+    }()
+    
     override func viewDidLoad() {
-        view.backgroundColor = .blue
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//        if let videoURL = selectedMember?.videoURL {
+        if let videoURL = videoURL {
+            player = AVPlayer(url: videoURL)
+            
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = view.bounds
+            view.layer.addSublayer(playerLayer)
+            
+            // Start playing the video automatically
+            player?.play()
+            
+            setupTimeObserver()
+            
+            player?.currentItem?.addObserver(self, forKeyPath: "status", options: [.new], context: nil)
+        }
+        
+        view.addSubview(timelineProgressBar)
+        setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        player?.pause() // Pause the player when the view is about to disappear
+        removeTimeObserver()
     }
-    */
-
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "status", let playerItem = player?.currentItem {
+            if playerItem.status == .readyToPlay {
+                // Start playing the video automatically
+                player?.play()
+            }
+        }
+    }
+    
+    func setupTimeObserver() {
+        let interval = CMTime(seconds: 0.05, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        
+        timeObserverToken = player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main) { [weak self] time in
+            guard let player = self?.player else { return }
+            let currentTime = CMTimeGetSeconds(player.currentTime())
+            let duration = CMTimeGetSeconds(player.currentItem?.duration ?? CMTime.zero)
+            let progress = Float(currentTime / duration)
+            
+            // Update the timeline progress bar UI
+            self?.timelineProgressBar.setProgress(progress, animated: true)
+            
+            if currentTime >= duration {
+                self?.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func removeTimeObserver() {
+        if let token = timeObserverToken {
+            player?.removeTimeObserver(token)
+            timeObserverToken = nil
+        }
+    }
+    
+    func setupConstraints() {
+        timelineProgressBar.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(4)
+        }
+    }
 }
